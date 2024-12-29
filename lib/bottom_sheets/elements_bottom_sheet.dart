156dce8abd397e33a5ab5e07cf8e32b745728171
Paras Jain <45650941+retroportalstudio@users.noreply.github.com>
2024-12-29 14:33:35 +0530

    Added Builder for Media Selector

--- lib/bottom_sheets/elements_bottom_sheet.dart
+++ lib/bottom_sheets/elements_bottom_sheet.dart
@@ -7,28 +7,44 @@ class WEElementsBottomSheet extends StatelessWidget {
   final TextStyle? titleTextStyle, itemTextStyle;
   final double? iconSize;
   final Color? iconColor;
+  final Function(Widget child)? mediaButtonBuilder;
   final WEquilEditorController controller;
   final Function()? onInsertHyperlink;
   final Function()? onInsertMedia;
   final Function()? onInsertVideoEmbed;
 
-  const WEElementsBottomSheet({super.key,
-    required this.controller,
-    this.title = "Insert",
-    this.titleTextStyle,
-    this.itemTextStyle,
-    this.iconSize,
-    this.iconColor,
-    this.onInsertHyperlink,
-    this.onInsertMedia,
-    this.onInsertVideoEmbed});
+  const WEElementsBottomSheet(
+      {super.key,
+      required this.controller,
+      this.title = "Insert",
+      this.titleTextStyle,
+      this.itemTextStyle,
+      this.iconSize,
+      this.iconColor,
+      this.onInsertHyperlink,
+      this.mediaButtonBuilder,
+      this.onInsertMedia,
+      this.onInsertVideoEmbed});
 
   @override
   Widget build(BuildContext context) {
-    final TextTheme textTheme = Theme
-        .of(context)
-        .textTheme;
+    final TextTheme textTheme = Theme.of(context).textTheme;
     const double defaultIconSize = 20;
+
+    final Widget mediaTile = ListTile(
+      dense: true,
+      onTap: () {
+        Navigator.of(context).pop();
+        onInsertMedia?.call();
+      },
+      contentPadding: EdgeInsets.zero,
+      leading: Icon(Icons.image,
+          size: iconSize ?? defaultIconSize, color: iconColor ?? Colors.black),
+      title: Text(
+        "Media",
+        style: itemTextStyle ?? textTheme.bodyMedium,
+      ),
+    );
 
     return Padding(
       padding: const EdgeInsets.all(20),
@@ -71,21 +87,9 @@ class WEElementsBottomSheet extends StatelessWidget {
           const Divider(
             height: 1,
           ),
-          ListTile(
-            dense: true,
-            onTap: () {
-              Navigator.of(context).pop();
-              onInsertMedia?.call();
-            },
-            contentPadding: EdgeInsets.zero,
-            leading: Icon(Icons.image,
-                size: iconSize ?? defaultIconSize,
-                color: iconColor ?? Colors.black),
-            title: Text(
-              "Media",
-              style: itemTextStyle ?? textTheme.bodyMedium,
-            ),
-          ),
+          mediaButtonBuilder == null
+              ? mediaTile
+              : mediaButtonBuilder?.call(mediaTile),
           const Divider(
             height: 1,
           ),
